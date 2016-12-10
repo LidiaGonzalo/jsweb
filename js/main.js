120604d103d0064415d4f6665d6afcc3e0f5c68b
LidiaGonzalo <lidiagonzalolopez@gmail.com>
2016-12-10 17:48:18 +0100

    botón papelera y función de borrado de alumnos. Maquetación tabla

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ var nUF1846=[5,5];
 $(document).ready(function($) {
     function cargarAlumnos() {
         for (var i = 0; i < nombres.length; i++) {
+            var notas = [nUF1841[i], nUF1842[i], nUF1843[i], nUF1844[i], nUF1845[i], nUF1846[i]];
             var html_text = "<tr>" +
                 "<td><input type='checkbox' value=''/> </td>" +
                 "<td>" + nombres[i] + "</td>" +
@@ -20,19 +21,29 @@ $(document).ready(function($) {
                 "<td>" + nUF1844[i] + "</td>" +
                 "<td>" + nUF1845[i] + "</td>" +
                 "<td>" + nUF1846[i] + "</td>" +
-                "<td></td>" +
+                "<td>"+calcularMedia(notas).toFixed(2) +"</td>" +
                 "<td class='editar'><a href='#'><i class='fa fa-pencil' aria-hidden='true'></i></a></td>" +
+                "<td class='borrar'><a class='btn-borrar' href='#'><i class='fa fa-trash' aria-hidden='true'></i></a></td>" +
                 "</tr>";
             $('#listado-alumnos tbody').append(html_text);
-
         }
     }
     cargarAlumnos();
     totalAumnos();
-
+    $("#listado-alumnos tr td .btn-borrar").on("click", function(e){
+          $(this).closest('tr').remove();
+    });
     function totalAumnos(){
         $("#listado-alumnos tr td:eq(0)").text("Total Alumnos: " + nombres.length);
     }
-
+    function calcularMedia(notas) {
+        var media = 0;
+        var n = notas.length;
+        for (var i = 0; i <n; i++) {
+            media += notas[i];
+        }
+        media = media / n;
+        return media;
+    }
 });
 
